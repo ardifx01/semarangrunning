b807fe3a553c4d35454361cf89b2422e5de532e4
SipjakiBlora <sipjakiblora@gmail.com>
2025-08-17 00:19:20 +0700

    erg

--- resources/views/login/register.blade.php
+++ resources/views/login/register.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="icon" href="/assets/abgblora/logo/racenewlogo.png" type="image/x-icon">
+  <link rel="icon" href="/assets/abgblora/logo/snocnew.png" type="image/x-icon">
 
     <title>Silahkan Login - SNOC X</title>
     <style>
